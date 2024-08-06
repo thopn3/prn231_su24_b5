5f73f07eebef2cfb3e2a06d476dc30803169f57b
ThoPN <thopn3@fpt.edu.vn>
2024-08-06 11:58:01 +0700

    Work DB in memory

---
+++ MyDodoAPI/Controllers/TodoItemController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyDodoAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyDodoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoItemController : Controller
+    {
+        private readonly TodoContext _context;
+        public TodoItemController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // POST api/todoitem
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+            // Tra 1 doi tuong todo vua luu vao db
+            return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+        }
+
+        // GET: api/todoitem
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems(List<TodoItem> todos)
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        // GET: api/todoitem/id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var result = await _context.TodoItems.FindAsync(id);
+            if (result != null)
+                return Ok(result);
+            return NotFound();
+        }
+
+    }
+}
+
---
+++ MyDodoAPI/Models/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyDodoAPI.Models
+{
+	// Class mo ta mo hinh CSDL, la 1 anh xa cua CSDL
+	public class TodoContext: DbContext
+	{
+		public TodoContext(DbContextOptions<TodoContext> options): base(options)
+		{
+		}
+
+		public DbSet<TodoItem> TodoItems { get; set; } = null;
+	}
+}
+
